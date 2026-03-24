679bc975240657b4aa2cf8891c9ef81ab669171d
brupokej <brupokej@gmail.com>
2026-03-24 09:19:52 -0400

    rename main title, images, links

--- site/src/components/HomepageFeatures/index.tsx
+++ site/src/components/HomepageFeatures/index.tsx
@@ -11,32 +11,32 @@ type FeatureItem = {
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: 'Title 1',
+    Svg: require('@site/static/img/logo.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec commodo
+        enim ut nisl lacinia, id viverra mauris facilisis.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    title: 'Title 2',
+    Svg: require('@site/static/img/logo.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Vestibulum ante felis, consequat ac sagittis eu, imperdiet a erat. Morbi
+        vitae erat eu nibh venenatis euismod non vel odio.
       </>
     ),
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: 'Title 3',
+    Svg: require('@site/static/img/logo.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Maecenas blandit fermentum maximus. Vestibulum volutpat consequat nulla
+        ac porttitor. Cras hendrerit ante nisi.
       </>
     ),
   },
--- site/src/pages/index.tsx
+++ site/src/pages/index.tsx
@@ -20,8 +20,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            to="/guide/intro">
+            Strategy Guide
           </Link>
         </div>
       </div>
@@ -32,9 +32,7 @@ function HomepageHeader() {
 export default function Home(): ReactNode {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+    <Layout title="Home" description={siteConfig.tagline}>
       <HomepageHeader />
       <main>
         <HomepageFeatures />
